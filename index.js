d265a5e8e33853a6d5d20e978e3e2f53c2ed005a
ashimroyrupok <ashimroyrupok@gmail.com>
2023-11-24 16:56:33 +0600

    started assignment 12

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+// app.use()
+
+
+app.get('/' ,  async(req,res) => {
+    res.send('server is running')
+})
+
+app.listen(port, ()=> {
+    console.log(`server is runnig on ${port}`);
+})
